4851838cf275ab259a87d4e2d582161ef23577cd
Renan Kosicki <renan@digi.ag>
2015-09-10 17:10:15 -0300

    project creation

---
+++ mara-shoes/mara-shoes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mara-shoes
+//
+//  Created by Renan Kosicki on 9/10/15.
+//  Copyright © 2015 Renan Kosicki | K-Mobi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mara-shoes/mara-shoes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mara-shoes
+//
+//  Created by Renan Kosicki on 9/10/15.
+//  Copyright © 2015 Renan Kosicki | K-Mobi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
